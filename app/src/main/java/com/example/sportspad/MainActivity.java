c1e8e53eede46a45d92133412751c89bb090a694
Timhrovat35 <Tim.Hrovat23@gmail.com>
2023-01-08 15:25:45 +0100

    Addded get api.

--- app/src/main/java/com/example/sportspad/MainActivity.java
+++ app/src/main/java/com/example/sportspad/MainActivity.java
@@ -1,14 +1,73 @@
 package com.example.sportspad;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RequestQueue requestQueue;
+    private TextView events;
+    private String url = "https://spordpad.azurewebsites.net/api/events";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        events = (TextView) findViewById(R.id.events);
     }
+
+    public void prikaziDogodke(View view){
+        if(view != null){
+            JsonArrayRequest request = new JsonArrayRequest(url, jsonArrayListener, errorListener);
+            requestQueue.add(request);
+        }
+    }
+
+    private Response.Listener<JSONArray> jsonArrayListener = new Response.Listener<JSONArray>() {
+        @Override
+        public void onResponse(JSONArray response) {
+            ArrayList<String> data = new ArrayList<>();
+
+            for (int i = 0; i < response.length(); i++){
+                try {
+                    JSONObject object = response.getJSONObject(i);
+                    String name = object.getString("name");
+                    String description = object.getString("description");
+
+                    data.add("Naslov: " + name + " , Opis: " + description);
+                }catch (JSONException e){
+                    e.printStackTrace();
+                    return;
+
+                }
+            }
+            for(String row: data){
+                String currentText = events.getText().toString();
+                events.setText(currentText + "\n\n" + row);
+            }
+        }
+    };
+    private Response.ErrorListener errorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.d("REST error", error.getMessage());
+        }
+    };
 }
